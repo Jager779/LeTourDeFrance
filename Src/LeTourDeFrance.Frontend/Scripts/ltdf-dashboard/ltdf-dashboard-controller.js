4ebc79dab4e45c6b7146c1c8d3bcc7a4a168f12e
Peter Holmberg <peter.hlmbrg@gmail.com>
2014-12-23 20:09:24 +0100

    Dashboard

---
+++ Src/LeTourDeFrance.Frontend/Scripts/ltdf-dashboard/ltdf-dashboard-controller.js
@@ -0,0 +1,10 @@
+﻿angular.module('ltdf').controller('ltdfDashboardController', [
+    '$scope', '$http',
+    function($scope, $http) {
+        $scope.model = {};
+        $scope.model.riders = [
+            { name: 'Fabio Cancellera', team: 'Trek Factyory Racing (TFR)' },
+            { name: 'Jens Voigt', team: 'Trek Factyory Racing (TFR)' }
+        ];
+    }
+]);
---
+++ Src/LeTourDeFrance.Frontend/Scripts/ltdf-dashboard/ltdf-dashboard.js
@@ -0,0 +1,10 @@
+﻿angular.module('ltdf').directive('ltdfDashboard', [
+    function () {
+        return {
+            restrict: 'AE',
+            controller: 'ltdfDashboardController',
+            templateUrl: '/Scripts/ltdf-dashboard/ltdf-dashboard.html'
+        };
+
+    }
+]);
