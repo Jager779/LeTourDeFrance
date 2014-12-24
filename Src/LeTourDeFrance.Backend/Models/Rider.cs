e3787a60365f22ce986778b75a50e553b8c411e5
Peter Holmberg <peter.hlmbrg@gmail.com>
2014-12-24 01:07:25 +0100

    Added id to riders

--- Src/LeTourDeFrance.Backend/Models/Rider.cs
+++ Src/LeTourDeFrance.Backend/Models/Rider.cs
@@ -1,5 +1,8 @@
-﻿namespace LeTourDeFrance.Backend.Models {
+﻿using System;
+
+namespace LeTourDeFrance.Backend.Models {
     public class Rider {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Country { get; set; }
         public string Team { get; set; }
