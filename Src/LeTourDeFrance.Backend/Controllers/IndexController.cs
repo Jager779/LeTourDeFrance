24bd0dd9e9710262c0f4b6820d49d42ac20191ea
Peter Holmberg <peter.hlmbrg@gmail.com>
2014-12-24 20:59:08 +0100

    Moved to single controller

--- Src/LeTourDeFrance.Backend/Controllers/IndexController.cs
+++ Src/LeTourDeFrance.Backend/Controllers/IndexController.cs
@@ -1,6 +1,24 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using LeTourDeFrance.Backend.Models;
+using LeTourDeFrance.Backend.Repositories;
 
 namespace LeTourDeFrance.Backend.Controllers {
     public class IndexController : ApiController {
+         private readonly IRepository _repository;
+
+        public IndexController(IRepository repository) {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Rider> GetAllRiders() {
+            return _repository.GetAllRiders();
+        }
+
+        [HttpGet]
+        public Rider GetRider(int number) {
+            return _repository.GetRider(number);
+        }
     }
 }
