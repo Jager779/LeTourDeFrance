7b30672ee90634c5ade5ea694aa3adec4c2cae16
Peter Holmberg <peter.hlmbrg@gmail.com>
2015-01-27 17:33:00 +0100

    Moved ajax from controller to service

--- Src/LeTourDeFrance.Frontend/Scripts/ltdf-dashboard/ltdf-dashboard-controller.js
+++ Src/LeTourDeFrance.Frontend/Scripts/ltdf-dashboard/ltdf-dashboard-controller.js
@@ -1,42 +1,38 @@
 ﻿angular.module('ltdf').controller('ltdfDashboardController', [
-    '$scope', '$http',
-    function($scope, $http) {
+    '$scope', '$http', 'ltdfDashboardService',
+    function($scope, $http, ltdfDashboardService) {
         $scope.model = {};
         $scope.model.imagePath = "http://localhost/LeTourDeFrance.Frontend/Content/Images/stage-1.jpg";
-        
 
 
-        (function ridersFromServer() {
-            $http.get('http://localhost/LeTourDeFrance.Backend/api/riders')
-                .then(function(data) {
-                        $scope.model.riders = data.data;
-                        console.log('yay');
-                        console.log(data);
-                    },
-                    function(data) {
+        (function() {
+            ltdfDashboardService.getRiders()
+                .success(function(data) {
+                    $scope.model.riders = data;
+                    console.log('yay');
+                    console.log(data);
+                })
+                .error(function(data) {
                         console.log('nay');
                     }
                 );
-        }());
-
-        (function stagesFromServer() {
-            $http.get('http://localhost/LeTourDeFrance.Backend/api/stages').
-                then(function(data) {
-                        $scope.model.stages = data.data;
-                        console.log('yay');
-                        console.log(data);
+            ltdfDashboardService.getStages().
+                success(function(data) {
+                    $scope.model.stages = data;
+                    console.log('yay');
+                    console.log(data);
                     $scope.model.selectedStage = $scope.model.stages[0];
-                },
-                    function(data) {
+                })
+                .error(function(data) {
                         console.log('nay');
-                    });
-            
-        }());
+                    }
+                );
+        })();
 
         $scope.loadImageOnClick = function(stage) {
             console.log("clicked stage: " + stage);
             $scope.model.imagePath = 'http://localhost/LeTourDeFrance.Frontend/Content/Images/stage-' + stage + '.jpg';
-            $scope.model.selectedStage = $scope.model.stages[stage -1];
+            $scope.model.selectedStage = $scope.model.stages[stage - 1];
             console.log($scope.model.selectedStage);
         };
     }
---
+++ Src/LeTourDeFrance.Frontend/Scripts/ltdf-dashboard/ltdf-dashboard-service.js
@@ -0,0 +1,14 @@
+﻿angular.module('ltdf').service('ltdfDashboardService', [
+    '$http',
+    function($http) {
+        var urlBase = 'http://localhost/LeTourDeFrance.Backend/api/';
+
+        this.getStages = function() {
+            return $http.get(urlBase + 'stages');
+        };
+
+        this.getRiders = function() {
+            return $http.get(urlBase + 'riders');
+        };
+    }
+]);
